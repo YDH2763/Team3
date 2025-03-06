3f732e90bf76c56fac2846928f630a4439bb3bc1
Soon17 <acejsh77@naver.com>
2025-03-06 18:49:13 +0900

    DB TRIGGER, PROCEDURE

--- DB/OMOK_PROJECT_DML.sql
+++ DB/OMOK_PROJECT_DML.sql
@@ -3,11 +3,10 @@ USE OMOK;
 INSERT INTO USER(U_NAME, U_PW) VALUES
 	("정순형", "1836"), ("박소은", "6974"),
     ("빅토리아", "5877"), ("김가람", "4485");
+
+INSERT ROOM(RO_NUM, RO_B_U_NAME, RO_W_U_NAME) VALUES
+	(15, "정순형", "박소은"), (1, "김가람", "빅토리아"),
+    (7, "정순형", "빅토리아"), (9, "김가람", "박소은");
     
-INSERT INTO RESULT(R_U_NUM, R_POSITION, R_OUTCOME) VALUES
-	(1, "BLACK", "WIN"), (2, "WHITE", "LOSE"), 
-    (1, "WHITE", "WIN"), (3, "BLACK", "LOSE"), 
-    (1, "WHITE", "DRAW"), (4, "BLACK", "DRAW"),
-    (2, "BLACK", "WIN"), (3, "WHITE", "LOSE"), 
-    (2, "WHITE", "WIN"), (4, "BLACK", "LOSE"), 
-    (3, "WHITE", "DRAW"), (4, "BLACK", "DRAW");
+INSERT INTO RESULT(RE_WINNER, RE_RO_ID) VALUES
+	("BLACK", 1), ("DRAW", 2), ("BLACK", 3), ("WHITE", 4);
