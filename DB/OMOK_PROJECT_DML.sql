d300f178c86f0d9ce0213f5d549a757d8c43f517
YDH2763 <dbehdgns12345@gmail.com>
2025-03-10 16:08:51 +0900

    Update OMOK_PROJECT_DML.sql

--- DB/OMOK_PROJECT_DML.sql
+++ DB/OMOK_PROJECT_DML.sql
@@ -5,8 +5,8 @@ INSERT INTO USER(U_NAME, U_PW) VALUES
     ("빅토리아", "5877"), ("김가람", "4485");
 
 INSERT ROOM(RO_NUM, RO_B_U_NAME, RO_W_U_NAME) VALUES
--- 	(15, "정순형", "박소은"), (1, "김가람", "빅토리아"),
---     (7, "정순형", "빅토리아"), (9, "김가람", "박소은"),
+	(15, "정순형", "박소은"), (1, "김가람", "빅토리아"),
+	(7, "정순형", "빅토리아"), (9, "김가람", "박소은"),
     (15, "박소은", "정순형"), (1,  "빅토리아","김가람"),
     (7, "빅토리아", "정순형"), (9, "박소은", "김가람");
     
@@ -14,6 +14,8 @@ INSERT INTO RESULT(RE_WINNER, RE_RO_ID) VALUES
 	("BLACK", 1), ("DRAW", 2), ("BLACK", 3), ("BLACK", 4),
 	("WHITE", 5), ("DRAW", 6), ("WHITE", 7), ("WHITE", 8);
 
-SELECT * FROM omok.user;    
+SELECT * FROM omok.user;
+SELECT * FROM omok.room;    
 SELECT * FROM omok.result;
 SELECT * FROM omok.score;
+SELECT * FROM omok.winning_rate;
